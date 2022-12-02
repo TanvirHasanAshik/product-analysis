2d3afcaf694e6c0f560d6585fe997f3575eb0517
TanvirHasanAshik <tanvirashik41@gmail.com>
2022-12-03 00:13:14 +0600

    redirects command done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="container">
+            <div className="row d-flex justify-content-center ">
+                <div className="col-md-12 col-12">
+                    <h1 className="fw-bold text-danger mt-5 pt-5">404 NOT FOUND</h1>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default NotFound;
